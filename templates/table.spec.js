55eca92c841aa8beeb0a4b8a436bbb16a69f7a4b
zoman <zomanhome@gmail.com>
2019-08-16 17:11:52 +0300

    Test added

--- templates/table.spec.js
+++ templates/table.spec.js
@@ -3,12 +3,11 @@ describe("myApp.table.controller", function() {
 
   beforeEach(function() {
     this.mockEmployee = jasmine.createSpyObj("employee", [
-      "delete",
-      "update",
-      "setContextView",
-      "setAction",
       "get",
-      "getCurrentContextMode"
+      "delete",
+      "getCurrentContextMode",
+      "setCurrentEmployee",
+      "setCurrentContextMode"
     ]);
   });
 
@@ -25,9 +24,37 @@ describe("myApp.table.controller", function() {
   });
 
   describe("delete", function() {
-    it("Delete", function() {
+    it("Delete employee from table", function() {
       this.scope.delete();
       expect(this.mockEmployee.delete).toHaveBeenCalled();
     });
   });
+
+  describe("editOrCreate", function() {
+    it("Edit employee from table row or create a new one", function() {
+      this.scope.editOrCreate();
+      expect(this.mockEmployee.setCurrentEmployee).toHaveBeenCalled();
+    });
+   });
+
+    describe("setContextView", function() {
+        it("Set context view", function() {
+            this.scope.setContextView();
+            //expect(this.mockEmployee.getCurrentContextMode).toHaveBeenCalled();
+            expect(this.mockEmployee.setCurrentContextMode).toHaveBeenCalled();
+        });
+    });
+
+    describe("setAction", function() {
+        it("Action with employee", function() {
+            expect(this.scope.setAction("delete")).toEqual(undefined);
+            expect(this.scope.setAction("edit")).toEqual(undefined);
+        });
+    });
+
+    describe("checkEmployeesLength", function() {
+        it("Set contextView true or false to depends on length", function() {
+            expect(this.scope.checkEmployeesLength()).toEqual(undefined);
+        });
+    });
 });
